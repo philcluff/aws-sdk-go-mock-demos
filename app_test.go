24fc592f3b0cd065f27894e2834896368d863b08
Phil Cluff <pcluff@brightcove.com>
2015-02-11 12:00:12 -0800

    Working implementation

--- app_test.go
+++ app_test.go
@@ -2,7 +2,8 @@ package app
 
 import (
 	"fmt"
-	//"github.com/GeneticGenesis/aws-sdk-go-mock-demos/mocks"
+	"github.com/GeneticGenesis/aws-sdk-go-mock-demos/mocks"
+	"github.com/awslabs/aws-sdk-go/aws"
 	"github.com/awslabs/aws-sdk-go/service/dynamodb"
 	"github.com/stretchr/testify/suite"
 	"testing"
@@ -26,10 +27,28 @@ func (s *AppSuite) SetupSuite() {
 
 func (s *AppSuite) TestHitDynamo() {
 
-	//client := new(mocks.DynamoDBer)
+	m := make(map[string]dynamodb.Condition)
+	var alist []dynamodb.AttributeValue
+
+	alist = append(alist, dynamodb.AttributeValue{
+		S: aws.String("1"),
+	})
+
+	m["Id"] = dynamodb.Condition{
+		AttributeValueList: alist,
+		ComparisonOperator: aws.String(dynamodb.ComparisonOperatorEq),
+	}
+
+	foo := &dynamodb.QueryInput{
+		KeyConditions: m,
+		TableName:     aws.String("foobar2"),
+	}
+
+	client := new(mocks.DynamoDBer)
+	client.On("Query", foo).Return(&dynamodb.QueryOutput{Count: aws.Integer(0)}, nil)
 
 	helper := &DynamoHelper{
-		dynamodb.New(nil),
+		client,
 	}
 	output, err := helper.HitDynamo("1")
 	fmt.Printf("Found: %v entries\n", *output.Count)
